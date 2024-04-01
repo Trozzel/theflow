cb3f18a64a2c1b439fc5bb654955cc63f032d0ad
George Ford <george.ford@icloud.com>
2024-03-31 23:30:07 -0400

    cli to add a context with a name

--- src/bin/show_contexts.rs
+++ src/bin/show_contexts.rs
@@ -1,7 +1,10 @@
-use theflow::cli::get_table_crud_cli;
+use theflow::cli::insert;
 
 fn main() {
-
-    let tmp = get_table_crud_cli();
-    tmp.display();
+    let stdin_args: Vec<String> = std::env::args().collect();
+    assert!(stdin_args.len() > 4 && stdin_args.len() % 2 == 1);
+    match insert(&stdin_args) {
+        Ok(num_insert) => println!("Successfully inserted {} records", num_insert),
+        Err(e) => panic!("Error inserting into table: {:?}", e)
+    }
 }
